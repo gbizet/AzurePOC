52fe3a2af5106f6471dbf6a8e47ab66a33980c28
BOUKEZZOULA Fouzi <fboukezzoula@finaxys.com>
2017-06-12 17:16:34 +0200

    Update xPrice.ps1

--- xPrice.ps1
+++ xPrice.ps1
@@ -1,70 +1,41 @@
-$global:ReturnsxPricerKeys = [System.Collections.ArrayList]@("")
-$template_json_file = "c:\template.json"
+#.\xPricer.ps1 -Purge force -AzureRmResourceGroup ompsg -AzureRmStorageAccount ompsg -AzureRmBatchAccount ompsg -PoolName ompsg
 
-Function global:Create-xPricer-Azure-Skeleton {
-
-    param(
+param(
     [String]$Purge,
     [String]$AzureRmResourceGroup,
     [String]$AzureRmStorageAccount,
-    [String]$AzureRmBatchAccount)
-
-    $Purge
-    $AzureRmResourceGroup
-    $AzureRmStorageAccount
-    $AzureRmBatchAccount
-
-       
-    if (($Purge -eq "force")) {    
-        Remove-AzureRmResourceGroup –Name "$AzureRmResourceGroup" -Force         
-    }     
-
-
-    #create a resource Group
-    New-AzureRmResourceGroup –Name "$AzureRmResourceGroup" –Location “West Europe” 
-
-    #create storage account
-    New-AzureRmStorageAccount –ResourceGroup "$AzureRmResourceGroup" –StorageAccountName "$AzureRmStorageAccount" –Location "West Europe" –Type "Standard_LRS" 
-
-    #create batch service
-    New-AzureRmBatchAccount –AccountName "$AzureRmBatchAccount" –Location "Central US" –ResourceGroupName "$AzureRmResourceGroup"
-
-}
-
-Function global:Returns-xPricer-Keys {
-
-    param([String]$AzureRmBatchAccount)
-
-    $Account = Get-AzureRmBatchAccountKeys –AccountName "$AzureRmBatchAccount"
-    $PrimaryAccountKey = $Account.PrimaryAccountKey
-    $SecondaryAccountKey = $Account.SecondaryAccountKey 
-    $global:ReturnsxPricerKeys.Add("$PrimaryAccountKey")
-    $global:ReturnsxPricerKeys.Add("$SecondaryAccountKey")
-
-}
-
-Function global:PoolCreation {
-
-    param([String]$AzureRmBatchAccount,
+    [String]$AzureRmBatchAccount,
     [String]$PoolName)
 
-    $context = Get-AzureRmBatchAccountKeys -AccountName "$AzureRmBatchAccount"
-    $configuration = New-Object -TypeName "Microsoft.Azure.Commands.Batch.Models.PSCloudServiceConfiguration" -ArgumentList @(4,"*")
-    New-AzureBatchPool -Id "$PoolName" -VirtualMachineSize "Small" -CloudServiceConfiguration $configuration -AutoScaleFormula '$TargetDedicated=4;' -BatchContext $context
-    Write-Host "Pool $PoolName has been created ..."
-}
-
-clear;
-
 Login-AzureRmAccount 
+   
+$global:ReturnsxPricerKeys = [System.Collections.ArrayList]@("")
+$template_json_file = "c:\template.json"
+       
+if (($Purge -eq "force")) {    
+Remove-AzureRmResourceGroup –Name "$AzureRmResourceGroup" -Force         
+}     
 
-Create-xPricer-Azure-Skeleton force xpricer xpricerstorageaccount xpricerbatchaccount
 
-Returns-xPricer-Keys xpricerbatchaccount
-$ReturnsxPricerKeys[0]
-$ReturnsxPricerKeys[1]
+#create a resource Group
+New-AzureRmResourceGroup –Name "$AzureRmResourceGroup" –Location “West Europe” 
 
-PoolCreation xpricerbatchaccount autoscale
+#create storage account
+New-AzureRmStorageAccount –ResourceGroup "$AzureRmResourceGroup" –StorageAccountName "$AzureRmStorageAccount" –Location "West Europe" –Type "Standard_LRS" 
+
+#create batch service
+New-AzureRmBatchAccount –AccountName "$AzureRmBatchAccount" –Location "Central US" –ResourceGroupName "$AzureRmResourceGroup"
+
+$Account = Get-AzureRmBatchAccountKeys –AccountName "$AzureRmBatchAccount"
+$PrimaryAccountKey = $Account.PrimaryAccountKey
+$SecondaryAccountKey = $Account.SecondaryAccountKey 
+$global:ReturnsxPricerKeys.Add("$PrimaryAccountKey")
+$global:ReturnsxPricerKeys.Add("$SecondaryAccountKey")
+
+$context = Get-AzureRmBatchAccountKeys -AccountName "$AzureRmBatchAccount"
+$configuration = New-Object -TypeName "Microsoft.Azure.Commands.Batch.Models.PSCloudServiceConfiguration" -ArgumentList @(4,"*")
+New-AzureBatchPool -Id "$PoolName" -VirtualMachineSize "Small" -CloudServiceConfiguration $configuration -AutoScaleFormula '$TargetDedicated=4;' -BatchContext $context
+Write-Host "Pool $PoolName has been created ..."
 
 $json = @"
 {
@@ -72,12 +43,14 @@ $json = @"
 }
 "@
 
-
 $jobj = ConvertFrom-Json -InputObject $json    
     
 $Key = $ReturnsxPricerKeys[1]
-$jobj | add-member "key" "$Key" -MemberType NoteProperty
-   
 
+$jobj | add-member "AzureRmStorageAccount" "$AzureRmStorageAccount" -MemberType NoteProperty
+$jobj | add-member "PrivateKey" "$Key" -MemberType NoteProperty
+$jobj | add-member "AzureRmBatchAccount" "$AzureRmBatchAccount" -MemberType NoteProperty
+$jobj | add-member "PoolName" "$PoolName" -MemberType NoteProperty
+   
 ConvertTo-Json $jobj | Out-File $template_json_file
 (Get-Content -path "$template_json_file" -Encoding Unicode) | Set-Content -Encoding "Default" -Path "$template_json_file"
