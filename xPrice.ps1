b4b64d0cb4c4d9ee7a7c592f2d32339067819a77
BOUKEZZOULA Fouzi <fboukezzoula@finaxys.com>
2017-06-12 16:09:31 +0200

    Create xPrice.ps1

---
+++ xPrice.ps1
@@ -0,0 +1,54 @@
+$global:ReturnsxPricerKeys = [System.Collections.ArrayList]@("")
+
+Function global:Create-xPricer-Azure-Skeleton {
+
+    param(
+    [String]$Purge,
+    [String]$AzureRmResourceGroup,
+    [String]$AzureRmStorageAccount,
+    [String]$AzureRmBatchAccount)
+
+    $Purge
+    $AzureRmResourceGroup
+    $AzureRmStorageAccount
+    $AzureRmBatchAccount
+
+       
+    if (($Purge -eq "force")) {    
+        Remove-AzureRmResourceGroup –Name "$AzureRmResourceGroup" -Force         
+    }     
+
+
+    #create a resource Group
+    New-AzureRmResourceGroup –Name "$AzureRmResourceGroup" –Location “West Europe” 
+
+    #create storage account
+    New-AzureRmStorageAccount –ResourceGroup "$AzureRmResourceGroup" –StorageAccountName "$AzureRmStorageAccount" –Location "West Europe" –Type "Standard_LRS" 
+
+    #create batch service
+    New-AzureRmBatchAccount –AccountName "$AzureRmBatchAccount" –Location "Central US" –ResourceGroupName "$AzureRmResourceGroup"
+
+}
+
+Function global:Returns-xPricer-Keys {
+
+    param([String]$AzureRmBatchAccount)
+
+    $Account = Get-AzureRmBatchAccountKeys –AccountName "$AzureRmBatchAccount"
+    $PrimaryAccountKey = $Account.PrimaryAccountKey
+    $SecondaryAccountKey = $Account.SecondaryAccountKey 
+    $global:ReturnsxPricerKeys.Add("$PrimaryAccountKey")
+    $global:ReturnsxPricerKeys.Add("$SecondaryAccountKey")
+
+}
+
+clear;
+
+Login-AzureRmAccount 
+
+Create-xPricer-Azure-Skeleton force xpricer xpricerstorageaccount xpricerbatchaccount
+
+Returns-xPricer-Keys xpricerbatchaccount
+$ReturnsxPricerKeys[0]
+$ReturnsxPricerKeys[1]
+
